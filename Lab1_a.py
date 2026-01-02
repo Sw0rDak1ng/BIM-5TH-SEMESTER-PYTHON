7c0b89a70ad2bd6d79f5db7aee3fe3ea7feac514
Swornim Regmi <regmiswornim2061@gmail.com>
2026-01-02 20:35:39 +0545

    New Commit

--- Lab1_a.py
+++ Lab1_a.py
@@ -3,12 +3,12 @@
 a = int(input("Enter first number:"))
 b = int(input("Enter second number:"))
 
-print("Addition:",a+b)
+print("The Add is:",a+b)
 
-print("Subtraction:",a-b)
+print("The Sub is:",a-b)
 
-print("Multiplication:",a*b)
+print("The Mul is:",a*b)
 
-print("Division:",a/b)
+print("The Div is:",a/b)
 
 

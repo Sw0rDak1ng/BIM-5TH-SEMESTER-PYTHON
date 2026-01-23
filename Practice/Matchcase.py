802d41298f3b04d4241743bd8bbdafa8318faf41
Swornim Regmi <regmiswornim2061@gmail.com>
2026-01-24 00:36:24 +0545

    New Commit

--- Practice/Matchcase.py
+++ Practice/Matchcase.py
@@ -1,3 +1,9 @@
-n=input('Enter an alphabet')
-match n:
-    
+value = input("Enter something: ")
+
+match value: 
+    case "a":
+        print("You entered a")
+    case "b":
+        print("You entered b")
+    case _:
+        print("You entered something else")
